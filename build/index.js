920331ebe52ac4d68626a8376621e060da86c895
Stanley Huang <huang.stanley050@gmail.com>
2019-10-09 19:31:33 +1100

    can sort string now

--- build/index.js
+++ build/index.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
+var CharactersCollection_1 = require("./CharactersCollection");
 var NumbersCollection_1 = require("./NumbersCollection");
 var array = new NumbersCollection_1.NumbersCollection([0, -4, 9, -2]);
-var sort = new Sorter_1.Sorter(array);
-sort.sort();
-console.log(sort.collection);
+var string = new CharactersCollection_1.CharactersCollection("Xaacbd");
+var sort_string = new Sorter_1.Sorter(string);
+var sort_number = new Sorter_1.Sorter(array);
+sort_string.sort();
+console.log(sort_string.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from "./Sorter";
+import { CharactersCollection } from "./CharactersCollection";
 import { NumbersCollection } from "./NumbersCollection";
 
 const array = new NumbersCollection([0, -4, 9, -2]);
-const sort = new Sorter(array);
-sort.sort();
-console.log(sort.collection);
+const string = new CharactersCollection("Xaacbd");
+const sort_string = new Sorter(string);
+const sort_number = new Sorter(array);
+
+sort_string.sort();
+sort_number.sort();
+console.log(sort_string.collection);
