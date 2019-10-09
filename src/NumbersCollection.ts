f494a73dcf04d6805f320a345787b0610268a1c9
Stanley Huang <huang.stanley050@gmail.com>
2019-10-09 17:56:14 +1100

    made swap, compare method

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,11 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+}
